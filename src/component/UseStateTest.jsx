995e9451d24604574829e09699f6c0b352fdf5fc
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-04 21:50:44 +0900

    prevStateの学習

--- src/component/UseStateTest.jsx
+++ src/component/UseStateTest.jsx
@@ -7,14 +7,59 @@ export default function UseStateTest() {
   //   return { value, onChange: (e) => setter(e.target.value) };
   // };
 
+  /**
+   * inputの扱い
+   */
   const [message, setMessage] = useState("初期値です");
   const changeMessage = (e) => {
     setMessage(e.target.value);
   };
+
+  /**
+   * count 系
+   */
+  const [count, setCount] = useState(0);
+  const countChange = (type) => {
+    setCount((prevState) => {
+      if (type === "up") return prevState + 1;
+      if (type === "down") return prevState - 1;
+    });
+  };
+
+  // アンチパターン
+  // count に直接変更を加えるのは良くない
+  const countChangeAntiPattern = (type) => {
+    setCount(() => {
+      if (type === "up") return count + 1;
+      if (type === "down") return count - 1;
+    });
+  };
+  console.log(countChangeAntiPattern);
+
   return (
     <div>
+      <h2>フォームのサンプル</h2>
       <input type="text" value={message} onChange={changeMessage} />
       <p>{message}</p>
+
+      <br />
+
+      <h2>カウントのサンプル</h2>
+      <h3>count:{count}</h3>
+      <button
+        onClick={() => {
+          countChange("up");
+        }}
+      >
+        up
+      </button>
+      <button
+        onClick={() => {
+          countChange("down");
+        }}
+      >
+        down
+      </button>
     </div>
   );
 }
